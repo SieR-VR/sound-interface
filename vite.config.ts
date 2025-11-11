1b85437d7b4561eb929c8a4d3122b5dd0fb80ffb
SieR-VR <me@sier-vr.dev>
2025-11-11 12:13:06 +0900

    fix: typo

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), svelte()],
-  base: "https://sier-vr.github.io/sound-intergace/",
+  base: "https://sier-vr.github.io/sound-interface/",
 });
