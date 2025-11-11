befa52c9e1b019cf0534e5317f4eaae0bd578595
SieR-VR <me@sier-vr.dev>
2025-11-11 12:11:35 +0900

    update title in index.html and format vite.config.ts for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()] });
+export default defineConfig({
+  plugins: [tailwindcss(), svelte()],
+  base: "https://sier-vr.github.io/sound-intergace/",
+});
